600b91c3722959db3b9ac7d9132e0b4fee321316
tomthechef.za@gmail.com <tomthechef.za@gmail.com>
2025-04-22 17:21:00 +0000

    int_orders_margin completed

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -5,6 +5,7 @@ SELECT
   , revenue
   , quantity
   , products.purchase_price
+  , ROUND(quantity*products.purchase_price,2) AS purchase_cost
   , Round(revenue - (cast(products.purchase_price as Float64)* quantity), 2) As Margin
 FROM {{ref('stg_raw__sales')}} 
  Join {{ref('stg_raw__product')}} as products
